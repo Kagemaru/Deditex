030c09f2998967325c5de06657f3259025e98df4
Thomas Burkhalter <thomas.burkhalter@kservices.ch>
2018-03-26 23:45:37 +0200

    Initial commit

---
+++ lib/deditex.ex
@@ -0,0 +1,18 @@
+defmodule Deditex do
+  @moduledoc """
+  Documentation for Deditex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Deditex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/deditex_test.exs
@@ -0,0 +1,8 @@
+defmodule DeditexTest do
+  use ExUnit.Case
+  doctest Deditex
+
+  test "greets the world" do
+    assert Deditex.hello() == :world
+  end
+end
